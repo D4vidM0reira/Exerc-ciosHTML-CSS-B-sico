1a7abbeabe64148245510c3be4bf1b485bfc8f0a
D4vidM0reira <davidsantosms49@gmail.com>
2023-08-14 21:56:36 -0400

    sincona e assincrona

--- Modulos - JS/Avançado/07-Parâmetro Rest/script.js
+++ Modulos - JS/Avançado/07-Parâmetro Rest/script.js
@@ -1,7 +1,12 @@
 function incentivarQuester(msg, ...quester) {
-    quester.map(quester => {
-        console.log(msg, quester);
-    })
+  quester.map((quester) => {
+    console.log(msg, quester);
+  });
 }
 
-incentivarQuester("parabéns por ter terminado o módulo de js avançado", 'joao', 'pedro', 'maria')
+incentivarQuester(
+  "parabéns por ter terminado o módulo de js avançado",
+  "joao",
+  "pedro",
+  "maria"
+);
